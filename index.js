2c45a7ff30b09bb0cb9d573e3847e9a108d57be9
[nayab aslam] <[aslamnayab56@gmail.com]>
2024-03-21 23:55:56 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,34 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const input1 = await inquirer.prompt({
+    message: "Enter first number",
+    type: "number",
+    name: "firstNumber"
+});
+const input2 = await inquirer.prompt({
+    message: "Enter Second number",
+    type: "number",
+    name: "secondNumber"
+});
+const input3 = await inquirer.prompt({
+    message: "Select any one operator to perform action",
+    type: "list",
+    name: "operator",
+    choices: ["Addition", "Subtraction", "Multiplication", "Division"]
+});
+//Conditional Statement
+if (input3.operator === "Addition") {
+    console.log(`Your answer is : ${input1.firstNumber + input2.secondNumber}`);
+}
+else if (input3.operator === "Subtraction") {
+    console.log(`Your answer is : ${input1.firstNumber - input2.secondNumber}`);
+}
+else if (input3.operator === "Multiplication") {
+    console.log(`Your answer is : ${input1.firstNumber * input2.secondNumber}`);
+}
+else if (input3.operator === "Division") {
+    console.log(`Your answer is : ${input1.firstNumber / input2.secondNumber}`);
+}
+else {
+    console.log("Please select valid operator");
+}
